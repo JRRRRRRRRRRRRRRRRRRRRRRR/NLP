68b4256b9fc899195c33132559e871a5e9b8aebb
KoenMoestuin <100768201+KoenMoestuin@users.noreply.github.com>
2023-12-13 23:39:23 +0100

    implemented topic matching, added hand picked songs

--- helper.py
+++ helper.py
@@ -97,6 +97,9 @@ def perform_sentiment_analysis(text):
     return average_score
 
 def extract_topics(text):
+    """
+    TODO
+    """
     topic_extraction_prompt = f"""
     Analyze the following text and extract 5 main topics.
     Each topic should be no more than two words.
@@ -147,3 +150,72 @@ def extract_topics(text):
         print("Response: ", response)
     print('Provided non-sense topics')
     return ['', '', '', '', '']
+
+
+def match_topics(topics_list, songs):
+    """
+    topics_list: List[str]. A list of topics provided by the user.
+    songs: List[dict]. A list of dictionaries, each representing a song and containing a list of topics.
+
+    ---
+    Returns tuple containing two elements:
+    - score_dict: Dict[int, float]. A dictionary where each key is an index from the songs list, and the value is the corresponding matching score.
+    - best_match: str. The title of the song that best matches the user's topics, identified by the highest score in score_dict.
+    """
+    dataset = ""
+    for i, song in enumerate(songs):
+        dataset += f"{i}. {str(song['topics'])} \n"
+    topic_matching_prompt = f"""
+    Given a user's list of topics and a dataset of songs with their respective topics,
+    calculate a matching score for each song based on the thematic similarity between the user's topics and the song's topics.
+    The score should range from 1 to 100, with 100 indicating a perfect match.
+
+    When evaluating similarity, consider not only exact matches but also related concepts. For example, if the user's topic is 'school', songs with topics like 'teacher', 'education', or 'learning' could also score high. Similarly, for a user's topic like 'game', related topics like 'soccer', 'play', or 'competition' might warrant higher scores.
+
+    Please provide the output directly in JSON format.
+    Each entry should consist of the song index as the key and its matching score as the value.
+    Also, include a separate entry named 'best_match' to indicate the index of the song that has the highest matching score with the user's topics.
+
+    User's Topics: "{str(topics_list)}"
+
+    Dataset of Songs and Their Topics:
+    {dataset}
+
+    Using the given guidelines, please provide a nuanced scoring for each song in the dataset, ensuring that the scores reflect both direct and thematic similarities.
+
+    I only need the JSON output showing the matching scores and the best match, without any additional code or explanation.
+    """
+    client = OpenAI()
+    try:
+        topics = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": topic_matching_prompt}
+            ],
+            temperature=0
+        )
+        response = topics.choices[0].message.content.strip()
+
+        # Extracting JSON part from the response
+        json_start = response.find('{')
+        json_end = response.rfind('}') + 1
+        if json_start != -1 and json_end != -1:
+            json_response = response[json_start:json_end]
+            score_dict = json.loads(json_response)
+            best_match_index = score_dict['best_match']
+            best_match = songs[best_match_index]['song_title']
+            return score_dict, best_match
+        else:
+            raise ValueError("No JSON response found in the output.")
+    except json.JSONDecodeError:
+        print("Error parsing JSON from the response.")
+        print("Response: ", response)
+    except (KeyError, ValueError) as e:
+        print(e)
+        print("Response: ", response)
+    except TypeError as e:
+        print(e)
+        print("Response: ", response)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        print("Response: ", response)
--- matching.py
+++ matching.py
@@ -1,6 +1,6 @@
 import json
 
-from helper import find_most_similar_song, perform_sentiment_analysis, extract_topics
+from helper import find_most_similar_song, perform_sentiment_analysis, extract_topics, match_topics
 
 
 # STEP 1: Load song lyrics and analysis from files
@@ -16,23 +16,23 @@ user_input_for_smanalysis = ''
 user_input_for_topic_modelling = ''
 
 # Ask three questions and store the answers
-question_1 = input("How are you doing?")
-user_input_for_smanalysis += question_1
+question_1 = input("How are you doing?\n")
+user_input_for_smanalysis += question_1 + ' '
 
-question_2 = input("If you were being completely honest with me, how would you describe your feelings lately?")
-user_input_for_smanalysis += question_2
+question_2 = input("If you were being completely honest with me, how would you describe your feelings lately?\n")
+user_input_for_smanalysis += question_2 + ' '
 
-question_3 = input("If you could choose a color to describe your current mood, what would it be?")#Colors are often associated with emotions (like blue for sadness, red for anger, yellow for happiness)
-user_input_for_smanalysis += question_3
+question_3 = input("If you could choose a color to describe your current mood, what would it be?\n")
+user_input_for_smanalysis += question_3 + ' '
 
-question_4 = input("What word would you use to describe your life right now?")
-user_input_for_topic_modelling += question_4
+question_4 = input("What word would you use to describe your life right now?\n")
+user_input_for_topic_modelling += question_4 + ' '
 
-question_5 = input("What did you do today?")
-user_input_for_topic_modelling += question_5
+question_5 = input("What did you do today?\n")
+user_input_for_topic_modelling += question_5 + ' '
 
-question_6 = input("What's a memory that has been influencing your emotions today?")#The memories they share can be a powerful indicator of their deeper feelings
-user_input_for_topic_modelling += question_6
+question_6 = input("What's a memory that has been influencing your emotions today?\n")
+user_input_for_topic_modelling += question_6 + ' '
 
 
 
@@ -41,23 +41,34 @@ user_input = user_input_for_smanalysis + user_input_for_topic_modelling
 
 
 # STEP 3: Find best fitting song based on embeddings
-most_similar_song, similarity_score = find_most_similar_song(songs, user_input)
+best_embedding_match, similarity_score = find_most_similar_song(songs, user_input)
 
 
 # STEP 4: Find best fitting song based on sentiment
 user_sentiment_score = perform_sentiment_analysis(user_input)
 
-current_song = None
 difference = 1000
 for song in sentimen_topics:
     sentiment_score = song['sentiment_score']
     if abs(sentiment_score - user_sentiment_score) < difference:
         difference = abs(sentiment_score - user_sentiment_score)
-        current_song = song['song_title']
+        best_sentiment_match = song['song_title']
 
 # STEP 5: Find best fitting song based on topic analysis
 # TODO Koen: implement topic matching
 topics = extract_topics(user_input_for_topic_modelling)
+print(topics)
+"""
+FOR TESTING:
+
+If you don't have an OpenAI API key, you can test with the following values.
+topics = ['smiling', 'school', 'horror movie', 'interview', 'athletics']
+score_dict = {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0, '31': 0, '32': 0, '33': 0, '34': 0, '35': 0, '36': 0, '37': 0, '38': 0, '39': 0, '40': 0, '41': 0, '42': 0, '43': 0, '44': 0, '45': 0, '46': 0, '47': 0, '48': 0, '49': 0, 'best_match': 0}
+best_topic_match = 'Childish-gambino-heartbeat'
+"""
+score_dict, best_topic_match = match_topics(topics, sentimen_topics)
+print(score_dict)
+print(best_topic_match)
 
 
 # STEP 6: Find best fitting song based on combination of embeddings, sentiment and topics
@@ -65,5 +76,6 @@ topics = extract_topics(user_input_for_topic_modelling)
 
 
 # STEP 7: Print the results
-print(f'based on embeddings, your song is: {most_similar_song}')
-print(f'based on sentiment, your song is: {current_song}')
+print(f'based on embeddings, your song is: {best_embedding_match}')
+print(f'based on sentiment, your song is: {best_sentiment_match}')
+print(f'based on topics, your song is: {best_topic_match}')
